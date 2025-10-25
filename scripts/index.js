5107ffe218fa3ca341e9c567c4aea55fbfd9b360
Fernando Salvador <ferydali@gmail.com>
2025-10-24 22:59:21 -0600

    feat - Crear un <template> en el HTML y eliminar las tarjetas existentes.

--- scripts/index.js
+++ scripts/index.js
@@ -11,35 +11,20 @@
   const descriptionInput = document.querySelector(".popup__input_type_description");
   
   // 1️⃣ Seleccionamos el formulario
-  const editProfileForm = document.querySelector("#edit-profile-form");
+const editProfileForm = document.querySelector("#edit-profile-form");
+  
+const cardsContainer = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
 
   console.log("creando la configuracion inicial");
   const initialCards = [
-    {
-      name: "Valle de Yosemite",
-      link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-    },
-    {
-      name: "Lago Louise",
-      link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-    },
-    {
-      name: "Montañas Calvas",
-      link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
-    },
-    {
-      name: "Latemar",
-      link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
-    },
-    {
-      name: "Parque Nacional de la Vanoise",
-      link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
-    },
+    { name: "Valle de Yosemite", link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg", },
+    { name: "Lago Louise", link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg", },
+    { name: "Montañas Calvas", link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg", },
+    { name: "Latemar", link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",},
+    { name: "Parque Nacional de la Vanoise", link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg", },
+    { name: "Lago di Braies", link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg", },
   ];
 
   initialCards.forEach(function (card) {
