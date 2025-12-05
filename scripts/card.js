963a386a0e45d71bb038ff10b851c80630101721
Fernando Salvador <ferydali@gmail.com>
2025-12-05 01:33:51 -0600

    feat - Se crear los metodos publicos

--- scripts/card.js
+++ scripts/card.js
@@ -13,7 +13,7 @@ export class Card {
   _getTemplate() {
     const cardElement = document
       .querySelector(this._cardSelector)
-      .content.querySelector('.card') // Asumiendo que el selector de tarjeta es '.card' dentro del template
+      .content.querySelector(".card") // Asumiendo que el selector de tarjeta es '.card' dentro del template
       .cloneNode(true);
 
     return cardElement;
@@ -21,7 +21,7 @@ export class Card {
 
   // 3. Método privado para el controlador de eventos "Like"
   _handleLikeIcon() {
-    this._likeButton.classList.toggle('card__like-button_active');
+    this._likeButton.classList.toggle("card__like-button_active");
   }
 
   // 4. Método privado para el controlador de eventos "Delete"
@@ -29,3 +29,44 @@ export class Card {
     // Elimina el elemento de la tarjeta del DOM
     this._element.remove();
     this._element = null; // Buena práctica para liberar la referencia
+  }
+
+  // 5. Método privado para configurar los detectores de eventos
+  _setEventListeners() {
+    // Botón de Like
+    this._likeButton = this._element.querySelector(".card__like-button");
+    this._likeButton.addEventListener("click", () => {
+      this._handleLikeIcon();
+    });
+
+    // Botón de Eliminar
+    this._deleteButton = this._element.querySelector(".card__delete-button");
+    this._deleteButton.addEventListener("click", () => {
+      this._handleDeleteCard();
+    });
+
+    // Imagen (para abrir la ventana modal con la imagen grande)
+    this._cardImage = this._element.querySelector(".card__image");
+    this._cardImage.addEventListener("click", () => {
+      // Usa el callback para manejar la apertura de la modal de imagen
+      this._handleImageClick(this._name, this._link);
+    });
+  }
+
+  // 6. Método público que devuelve el elemento card completamente funcional
+  generateCard() {
+    // Obtiene el elemento de la plantilla
+    this._element = this._getTemplate();
+
+    // Rellena la tarjeta con datos
+    this._element.querySelector(".card__title").textContent = this._name;
+    this._cardImage.src = this._link;
+    this._cardImage.alt = this._name; // Se añade 'alt' para accesibilidad
+
+    // Configura los eventos
+    this._setEventListeners();
+
+    // Devuelve el elemento final
+    return this._element;
+  }
+}
