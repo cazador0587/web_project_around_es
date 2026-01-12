3120e182196ba227430f05e2ab1ddfbada216b0a
Fernando Salvador <fsalvador@ecosistemasmexico.com>
2026-01-12 16:01:09 -0600

    feat - se crea el archivo constants.js en el que se guardara la configuracion del token

---
+++ scripts/constants.js
@@ -0,0 +1,7 @@
+const api = new Api({
+  baseUrl: "https://around-api.es.tripleten-services.com/v1",
+  headers: {
+    authorization: "b0f25e10-87c1-40a8-a2e9-343c7574fe36",
+    "Content-Type": "application/json",
+  },
+});
